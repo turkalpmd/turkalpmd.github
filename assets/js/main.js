b34d2012ff1090ec0b8ef12d22652d566bd0529b
turkalpmd <85236337+turkalpmd@users.noreply.github.com>
2022-08-24 22:44:29 +0300

    m

--- assets/js/main.js
+++ assets/js/main.js
@@ -122,18 +122,16 @@
   /**
    * Hero type effect
    */
-  const typed = select('.typed')
-  if (typed) {
-    let typed_strings = typed.getAttribute('data-typed-items')
-    typed_strings = typed_strings.split(',')
-    new Typed('.typed', {
-      strings: typed_strings,
-      loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000
-    });
-  }
+
+
+const hero = document.querySelector('.hero');
+
+hero.addEventListener('mousemove', (e) => {
+	let x = e.clientX;
+	let y = e.clientY;
+	hero.style.backgroundColor = `rgba(${0 + (x*0.05)}, ${90 + (y*0.05)}, 135, 0.8)`;
+	// console.log(x + ',' + y);
+})
 
   /**
    * Skills animation
